d2e0bd610a5ec929b19a6034d18bd689a15f573a
Asim Can Yagiz <meycasim@gmail.com>
2023-08-02 12:54:16 +0300

    Updated buffer function now if there is a error this function return nil

--- Sources/BackgroundRemoval/BackgroundRemoval.swift
+++ Sources/BackgroundRemoval/BackgroundRemoval.swift
@@ -24,26 +24,26 @@ public struct BackgroundRemoval {
     ///@param uploadedImage of the input image
     ///@param filterSharpness tha sharpness of filter if needed (recommeneded)
     ///@param maskOnly pass true if you want the mask onl, not the output image
-
+    
     public func removeBackground(image: UIImage, maskOnly: Bool = false) throws -> UIImage {
         let w = image.size.width
         let h = image.size.height
-
+        
         /// determine whether width or height is greater
         let longer = max(w, h)
         /// create a Square size box
         let sz = CGSize(width: longer, height: longer)
-
+        
         /// call scaling function to scale the image to the Square dimensions, using "aspect fit"
         guard let scaledImage = image.scaled(to: sz, scalingMode: .aspectFit) else {
             throw ImageProcessingError.scalingError
         }
-
+        
         /// resize image to 320 * 320 before sending it to the model
         guard let resize =  scaledImage.resizeImage(width: 320, height: 320) else {
             throw ImageProcessingError.sizingError
         }
-            
+        
         /// init model and get result
         guard let model = try? LaLabsu2netp.init(),
               let bufferResized = buffer(from: resize),
@@ -51,7 +51,7 @@ public struct BackgroundRemoval {
               let out = UIImage(pixelBuffer: result.out_p1) else {
             throw ImageProcessingError.processingError
         }
-
+        
         guard let scaledOut = out.scaled(to: sz, scalingMode: .aspectFit),
               let invertedOut = scaledOut.invertedImage() else {
             throw ImageProcessingError.inversionError
@@ -63,31 +63,39 @@ public struct BackgroundRemoval {
         
         return maskOnly ? scaledOut : finalResult
     }
-
+    
     
     func buffer(from image: UIImage) -> CVPixelBuffer? {
-      let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-      var pixelBuffer : CVPixelBuffer?
-      let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-      guard (status == kCVReturnSuccess) else {
-        return nil
-      }
-
-      CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-      let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-
-      let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-      let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-
-      context?.translateBy(x: 0, y: image.size.height)
-      context?.scaleBy(x: 1.0, y: -1.0)
-
-      UIGraphicsPushContext(context!)
-      image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-      UIGraphicsPopContext()
-      CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-
-      return pixelBuffer
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        guard let pixelBufferUnwrapped = pixelBuffer else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBufferUnwrapped, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBufferUnwrapped)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        guard let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBufferUnwrapped), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            return nil
+        }
+        
+        context.translateBy(x: 0, y: image.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBufferUnwrapped, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBufferUnwrapped
     }
-
+    
+    
 }
